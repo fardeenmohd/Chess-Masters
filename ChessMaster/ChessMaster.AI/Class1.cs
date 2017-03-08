45f62402c35657e14fd0cb758de031bb4ab035c0
Dell-Note\Иван <ivan070297@gmail.com>
2017-03-08 19:37:15 +0200

    Initial design of the ChessMaster

---
+++ ChessMaster/ChessMaster.AI/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMaster.AI
+{
+
+    /// <summary>
+    /// The minmax implementation
+    /// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ ChessMaster/ChessMaster/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChessMaster.Pieces;
+using ChessMaster.AI;
+
+namespace ChessMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// The GUI of the ChessMaster
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
