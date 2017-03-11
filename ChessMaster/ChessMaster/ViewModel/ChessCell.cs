1c4bd2ec97c13cc918a55bf8c131b899ecd8ecc6
Dell-Note\Иван <ivan070297@gmail.com>
2017-03-12 01:19:04 +0200

    Added base class for pieces

--- ChessMaster/ChessMaster/ViewModel/ChessCell.cs
+++ ChessMaster/ChessMaster/ViewModel/ChessCell.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using ChessMaster.Pieces;
+using System.Windows;
 
 namespace ChessMaster.ViewModel
 {
@@ -12,6 +14,12 @@ namespace ChessMaster.ViewModel
     /// </summary>
     public class ChessCell
     {
+        public Point Position { get; set; }
+
         public SolidColorBrush Background { get; set; }
+
+        public SolidColorBrush BorderColor { get; set; }
+
+        public BasePiece Piece { get; set; }
     }
 }
